8180f5c284387c919425cc0450888b919fcb0f09
Ryota Arai <ryota-arai@cookpad.com>
2014-08-19 22:18:46 +0900

    Update.

--- app/controllers/irkit_controller.rb
+++ app/controllers/irkit_controller.rb
@@ -1,5 +1,18 @@
 class IrkitController < ApplicationController
   def index
+    unless deviceid && clientkey
+      render text: "Please set IRKIT_DEVICEID and IRKIT_CLIENTKEY"
+      return
+    end
+  end
 
+  private
+
+  def deviceid
+    ENV['IRKIT_DEVICEID']
+  end
+
+  def clientkey
+    ENV['IRKIT_CLIENTKEY']
   end
 end
